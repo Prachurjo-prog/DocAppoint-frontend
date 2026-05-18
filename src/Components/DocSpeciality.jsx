69dfddc7acf41d8390cec36503e8f610279310f3
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-05-18 17:15:54 +0600

    Footer section added

---
+++ src/Components/DocSpeciality.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import general from "../../public/assets/General.png"
+
+
+const DocSpeciality = () => {
+    return (
+        <div className="container mx-auto md:pt-8">
+            <h1 className="md:text-4xl text-3xl font-semibold text-center md:pb-4 pb-1.5">Find by Speciality</h1>
+            <p className="text-center md:font-medium text-gray-600">Simply browse through our extensive list of trusted doctors, schedule your <br/> appointment hassle-free.</p>
+
+            
+
+        </div>
+    );
+};
+
+export default DocSpeciality;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "@/Components/Banner";
+import DocSpeciality from "@/Components/DocSpeciality";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
      <HeroSection/>
+     <DocSpeciality/>
     </div>
   );
 }
